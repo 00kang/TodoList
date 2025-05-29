80b9f8ed2507c1d57d21b6ed00f4354abff55961
00kang <kdb070970@gmail.com>
2025-05-30 01:06:51 +0900

    feat: 공통 TodoItem 컴포넌트 구현
    - 상태(Default, Active)에 따라 아이콘, 배경색, 텍스트 스타일 분기 렌더링

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+type TodoItemState = "Default" | "Active";
+
+interface TodoItemProps {
+  text: string;
+  state: TodoItemState;
+  onToggle: () => void;
+}
+
+export default function TodoItem({ text, state, onToggle }: TodoItemProps) {
+  const STYLE_MAP: Record<
+    TodoItemState,
+    {
+      icon: string;
+      bgColor: string;
+      textStyle: string;
+    }
+  > = {
+    Default: {
+      icon: "/images/icons/checkbox-todo.svg",
+      bgColor: "bg-white",
+      textStyle: "",
+    },
+    Active: {
+      icon: "/images/icons/checkbox-done.svg",
+      bgColor: "bg-violet-100",
+      textStyle: "line-through",
+    },
+  };
+
+  const { icon, bgColor, textStyle } = STYLE_MAP[state];
+
+  return (
+    <div
+      className={`flex h-[50px] w-full items-center gap-4 rounded-full border-2 border-slate-900 px-3 ${bgColor}`}
+    >
+      <button onClick={onToggle}>
+        <Image src={icon} alt="checkbox" width={32} height={32} />
+      </button>
+      <span
+        className={`text-base font-normal leading-none text-slate-900 ${textStyle}`}
+      >
+        {text}
+      </span>
+    </div>
+  );
+}
