ff3e876a06bbcb9a8ba0d2655ccdb7b9b331fd57
00kang <kdb070970@gmail.com>
2025-06-02 18:01:06 +0900

    feat: TodoHeader 컴포넌트 입력값에 따라 너비 자동 조절 기능 추가

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 type TodoHeaderState = "Default" | "Active";
 
@@ -8,12 +9,14 @@ interface TodoHeaderProps {
   text: string;
   state: TodoHeaderState;
   onToggle: () => void;
+  onTextChange: (newText: string) => void;
 }
 
 export default function TodoHeader({
   text,
   state,
   onToggle = () => {},
+  onTextChange,
 }: TodoHeaderProps) {
   const STYLE_MAP: Record<
     TodoHeaderState,
@@ -34,6 +37,17 @@ export default function TodoHeader({
 
   const { icon, bgColor } = STYLE_MAP[state];
 
+  const spanRef = useRef<HTMLSpanElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputWidth, setInputWidth] = useState(0);
+
+  useEffect(() => {
+    if (spanRef.current) {
+      const width = spanRef.current.offsetWidth;
+      setInputWidth(width + 8);
+    }
+  }, [text]);
+
   return (
     <div
       className={`flex h-[64px] w-full items-center justify-center gap-4 rounded-3xl border-2 border-slate-900 px-3 ${bgColor}`}
@@ -41,9 +55,22 @@ export default function TodoHeader({
       <button onClick={onToggle} className="flex items-center justify-center">
         <Image src={icon} alt="checkbox" width={32} height={32} />
       </button>
-      <span className="text-base font-bold leading-none text-slate-900">
-        {text}
-      </span>
+
+      <div className="relative">
+        <input
+          ref={inputRef}
+          className="bg-transparent text-base font-bold leading-none text-slate-900 underline focus:outline-none"
+          style={{ width: `${inputWidth}px` }}
+          value={text}
+          onChange={(e) => onTextChange(e.target.value)}
+        />
+        <span
+          ref={spanRef}
+          className="invisible absolute left-0 top-0 whitespace-pre text-base font-bold"
+        >
+          {text || "제목 없음"}
+        </span>
+      </div>
     </div>
   );
 }
